12d5fd1c0f566d0989c9b8832cd07bbeaa0790d4
Daniele Ugoletti <daniele.ugoletti@gruppometa.it>
2020-08-25 18:25:11 +0200

    Fix image in trix field

--- src/Fields/FieldCollection.php
+++ src/Fields/FieldCollection.php
@@ -26,6 +26,25 @@ class FieldCollection extends BaseFieldCollection
     }
 
     /**
+     * Find a given field by its attribute.
+     *
+     * @param string $attribute
+     * @param mixed $default
+     * @return \Laravel\Nova\Fields\Field|null
+     */
+    public function findFieldByAttribute($attribute, $default = null)
+    {
+        if ($field = $this->checkMatchTrixAttachment($default)) {
+            return $field;
+        }
+
+        return $this->first(function ($field) use ($attribute) {
+            return isset($field->attribute) &&
+                $field->attribute == $attribute;
+        }, $default);
+    }
+
+    /**
      * @param  string  $type
      * @return static
      */
@@ -51,6 +70,33 @@ class FieldCollection extends BaseFieldCollection
 
                 return $result;
             });
+        }
+
+        return null;
+    }
+
+    /**
+     * @param mixed $default
+     * @return static
+     */
+    private function checkMatchTrixAttachment($default)
+    {
+        preg_match('/\/.*\/trix-attachment\/[a-zA-z0-9]{16}__(.*)/', request()->path(), $match);
+        if (in_array(request()->method(), ['POST', 'DELETE']) && count($match)) {
+            return $this->filter(function ($value) {
+                return $value instanceof Flexible;
+            })->reduce(function ($carry, $item) {
+                $item->layouts()->each(function($item) use ($carry) {
+                    collect($item->fields())->each(function($item) use ($carry) {
+                        $carry->push($item);
+                    });
+                });
+
+                return $carry;
+            }, collect([]))
+            ->first(function ($value) use ($match) {
+                return $value->attribute==$match[1];
+            }, $default);
         }
 
         return null;
